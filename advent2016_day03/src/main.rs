44f43ace3b88c869183867c823e5edb473ada3e2
Jakub Gedeon <jgedeon@sofi.com>
2016-12-11 23:24:06 -0700

    d3p2

--- advent2016_day03/src/main.rs
+++ advent2016_day03/src/main.rs
@@ -32,20 +32,36 @@ fn read_file_lines() -> Lines<BufReader<File>> {
     BufRead::lines(reader)
 }
 
-fn solve1() -> usize {
-    read_file_lines()
-        .map(|s| parse_line(&s.unwrap()).unwrap())
+fn get_triangles() -> Vec<Shape> {
+    read_file_lines().map(|s| parse_line(&s.unwrap()).unwrap()).collect()
+}
+
+fn solve1(input: &[Shape]) -> usize {
+    input.iter().filter(|s| triangle(&s)).count()
+}
+
+fn solve2(input: &[Shape]) -> usize {
+    input
+        .chunks(3)
+        .flat_map(|shapes| {
+            shapes[0].iter()
+                .zip(shapes[1].iter())
+                .zip(shapes[2].iter())
+                .map(|((&a, &b), &c)| [a, b, c])
+        })
         .filter(triangle)
         .count()
 }
 
 fn main() {
-    println!("{}", solve1());
+    let input = get_triangles();
+    println!("Part 1: {}", solve1(&input));
+    println!("Part 2: {}", solve2(&input));
 }
 
 #[cfg(test)]
 mod tests {
-    use super::{parse_line, triangle};
+    use super::{parse_line, triangle, solve1, solve2};
 
     #[test]
     fn parse() {
@@ -56,5 +72,18 @@ mod tests {
     fn check() {
         assert!(triangle(&[3,4,5]));
         assert!(!triangle(&[5,10,25]));
+    }
+
+    #[test]
+    fn example() {
+        let input = [
+            [101, 301, 501],
+            [102, 302, 502],
+            [103, 303, 503],
+            [201, 401, 601],
+            [202, 402, 602],
+            [203, 403, 603]];
+        assert_eq!(solve1(&input), 3);
+        assert_eq!(solve2(&input), 6);
     }
 }
