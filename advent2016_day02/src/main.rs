027c527cb247ef650369c2168617c18c1d4479df
Jakub Gedeon <jgedeon@sofi.com>
2016-12-11 18:41:02 -0700

    d2p2

--- advent2016_day02/src/main.rs
+++ advent2016_day02/src/main.rs
@@ -13,6 +13,26 @@ fn keypad1((x, y): Pos) -> Option<Key> {
         .map(|c| c.clone())
 }
 
+const KEYPAD2: [[Key; 5]; 5] = [
+    ['0','0','1','0','0'],
+    ['0','2','3','4','0'],
+    ['5','6','7','8','9'],
+    ['0','A','B','C','0'],
+    ['0','0','D','0','0']];
+
+fn keypad2((x, y): Pos) -> Option<Key> {
+    KEYPAD2
+        .get(y as usize)
+        .and_then(|row| row.get(x as usize))
+        .and_then(|&c| {
+            if c != '0' {
+                Some(c)
+            } else {
+                None
+            }
+        })
+}
+
 fn mov(dir: char, (x, y): Pos) -> Pos {
     match dir {
         'L' => (x-1, y),
@@ -23,20 +43,28 @@ fn mov(dir: char, (x, y): Pos) -> Pos {
     }
 }
 
-fn solve1(path: &str) -> String {
-    let mut pos = (1, 1);
+fn solve<F: Fn(Pos) -> Option<Key>>(start: Pos, keypad: F, path: &str) -> String {
+    let mut pos = start;
     let mut result = String::new();
     for step in path.chars() {
         if step == '\n' {
-            result.push(keypad1(pos).unwrap());
+            result.push(keypad(pos).unwrap());
         } else {
             let next = mov(step, pos);
-            if let Some(_) = keypad1(next) {
+            if let Some(_) = keypad(next) {
                 pos = next;
             }
         }
     }
     result
+}
+
+fn solve1(path: &str) -> String {
+    solve((1,1), keypad1, path)
+}
+
+fn solve2(path: &str) -> String {
+    solve((0,2), keypad2, path)
 }
 
 fn read_file_param() -> String {
@@ -52,11 +80,12 @@ fn read_file_param() -> String {
 fn main() {
     let input = read_file_param();
     println!("Part 1: {}", solve1(&input));
+    println!("Part 2: {}", solve2(&input));
 }
 
 #[cfg(test)]
 mod tests {
-    use super::{keypad1, solve1};
+    use super::{keypad1, solve1, solve2};
 
     #[test]
     fn translation() {
@@ -69,5 +98,10 @@ mod tests {
     #[test]
     fn example1() {
         assert_eq!(&solve1(EXAMPLE), "1985");
+    }
+
+    #[test]
+    fn example2() {
+        assert_eq!(&solve2(EXAMPLE), "5DB3");
     }
 }
