c769308cd602f62d68b3d3c2ba3632481a8dfa16
Jakub Gedeon <jgedeon@sofi.com>
2016-12-17 21:26:58 -0700

    d5p2

--- advent2016_day05/src/main.rs
+++ advent2016_day05/src/main.rs
@@ -1,15 +1,20 @@
 extern crate md5;
-use md5::Context;
+use md5::{Context, Digest};
 
 use std::iter::Iterator;
 use std::io::Write;
 
+#[inline]
 fn starts_with_20_zeros(digest: &[u8; 16]) -> bool {
     digest[0] as u32 + digest[1] as u32 + (digest[2] >> 4) as u32 == 0
 }
 
 fn sixth_hex(digest: &[u8; 16]) -> u8 {
     digest[2] & 0x0f
+}
+
+fn seventh_hex(digest: &[u8; 16]) -> u8 {
+    digest[3] >> 4
 }
 
 fn to_hex(input: u8) -> char {
@@ -35,32 +40,72 @@ fn to_hex(input: u8) -> char {
     }
 }
 
-fn solve(input: &[u8], limit: usize) -> String {
-    let base = {
-        let mut init = Context::new();
-        init.write_all(input).unwrap();
-        init
-    };
-    let mut full = Context::new();
-    (0..).filter_map(|n| {
-        full.clone_from(&base);
-        full.write_all(n.to_string().as_bytes()).unwrap();
-        let digest = full.compute();
-        if starts_with_20_zeros(&digest) {
-            Some(sixth_hex(&digest))
-        } else {
-            None
+struct Digested {
+    iter: u64,
+    base: Context,
+}
+
+impl Digested {
+    fn from(input: &[u8]) -> Self {
+        Digested {
+            iter: 0,
+            base: {
+                let mut init = Context::new();
+                init.write_all(input).unwrap();
+                init
+            },
         }
-    }).take(limit).map(to_hex).collect()
+    }
+}
+
+impl Iterator for Digested {
+    type Item = Digest;
+
+    #[inline]
+    fn next(&mut self) -> Option<Digest> {
+        let mut full = self.base.clone();
+        full.write_all(self.iter.to_string().as_bytes()).unwrap();
+        self.iter += 1;
+        Some(full.compute())
+    }
+}
+
+fn solve1(input: &[u8], limit: usize) -> String {
+    Digested::from(input)
+        .filter(|d| starts_with_20_zeros(&d))
+        .map(|d| sixth_hex(&d))
+        .take(limit)
+        .map(to_hex)
+        .collect()
+}
+
+const EMPTY: u8 = 255;
+
+fn solve2(input: &[u8], limit: u8) -> String {
+    let mut buffer: Vec<u8> = (0..limit).map(|_| EMPTY).collect();
+    for digest in Digested::from(input).filter(|d| starts_with_20_zeros(&d)) {
+        let index = sixth_hex(&digest);
+        if index < limit && buffer[index as usize] == EMPTY {
+            let value = seventh_hex(&digest);
+            // println!("placed {} at {}", index, value);
+            buffer[index as usize] = value;
+            if buffer.iter().cloned().all(|cell| cell != EMPTY) {
+                break;
+            }
+        }
+    }
+    buffer.iter().cloned().map(to_hex).collect()
 }
 
 fn main() {
-    println!("{}", solve(b"cxdnnyjw", 8));
+    let input = b"cxdnnyjw";
+    println!("Part 1: {}", solve1(input, 8));
+    println!("Part 2: {}", solve2(input, 8));
 }
 
 #[cfg(test)]
 mod tests {
-    use super::{solve, starts_with_20_zeros};
+    use super::{solve1, solve2, starts_with_20_zeros};
     use md5::Context;
     use std::io::Write;
 
@@ -76,9 +121,13 @@ mod tests {
         assert!(starts_with_20_zeros(&temp.compute()));
     }
 
+    #[test]
+    fn example1() {
+        assert_eq!(&solve1(b"abc", 8), "18f47a30")
+    }
 
     #[test]
-    fn example() {
-        assert_eq!(&solve(b"abc", 8), "18f47a30")
+    fn example2() {
+        assert_eq!(&solve2(b"abc", 8), "05ace8e3")
     }
 }
